fa2bfa20b3ccf587e48dd372acaea87d6785ffc4
Eli Reiners <goelyukim@gmail.com>
2020-08-09 12:26:54 -0400

    Add row for a user with two subjects

--- seeds/seed.tutors_app_tutors_subjects.sql
+++ seeds/seed.tutors_app_tutors_subjects.sql
@@ -1,4 +1,5 @@
 insert into tutors_subjects (subjects_id, user_id) values (1, 1);
+insert into tutors_subjects (subjects_id, user_id) values (3, 1);
 insert into tutors_subjects (subjects_id, user_id) values (2, 2);
 insert into tutors_subjects (subjects_id, user_id) values (3, 3);
 insert into tutors_subjects (subjects_id, user_id) values (4, 4);
