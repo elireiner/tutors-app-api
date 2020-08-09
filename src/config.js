51afe7a229d4144c8ad0993e5909c15e113f4ae1
Eli Reiners <goelyukim@gmail.com>
2020-08-09 12:28:36 -0400

    Update to link to database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://example: dunder_mifflin@localhost/blogful',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://example: dunder_mifflin@localhost/blogful-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/tutors_app',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/tutors_app_test',
 }
