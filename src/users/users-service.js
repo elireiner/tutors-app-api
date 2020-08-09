50e378ccbd9f5924ccb9d84a1c3bcbd8927530fc
Eli Reiners <goelyukim@gmail.com>
2020-08-09 12:30:02 -0400

    Update to corospond with is column name

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -14,18 +14,18 @@ const UsersService = {
     },
 
     getById(knex, id) {
-        return knex.from('users').select('*').where('id', id).first()
+        return knex.from('users').select('*').where(' user_id', id).first()
     },
 
     deleteUser(knex, id) {
         return knex('users')
-            .where({ id })
+            .where({  'user_id': id })
             .delete()
     },
 
     updateUser(knex, id, newUserFields) {
         return knex('users')
-            .where({ id })
+            .where({ 'user_id': id  })
             .update(newUserFields)
     },
 }
